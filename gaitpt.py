ef488bb613c39cd05b3a3e7792298376516cd58c
Kayala47 <kjaa2018@mymail.pomona.edu>
2022-01-10 15:42:01 -0800

    added 3 legs to animat, total 4; changed walk() so that all four legs would move and so that it would instead return a list of frames, each w info on one step for all four legs

--- gaitpt.py
+++ gaitpt.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from itertools import accumulate
 from loguru import logger
 from math import atan2, cos, inf, pi, sin, sqrt, radians, degrees
-from typing import Iterable
+from typing import Iterable, List, Tuple
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -72,13 +72,16 @@ class Animat:
             (deg(20), deg(160)),
         ]
         front_leg_lengths = [0.26, 0.42, 0.32]
+        self.front_left = Leg(front_leg_angles, front_leg_limits, front_leg_lengths)
+        self.front_right = Leg(front_leg_angles, front_leg_limits, front_leg_lengths)
 
         rear_leg_angles = front_leg_angles
         rear_leg_limits = front_leg_limits
         rear_leg_lengths = [0.41, 0.42, 0.17]
 
         self.rear_left = Leg(rear_leg_angles, rear_leg_limits, rear_leg_lengths)
-        self.legs = [self.rear_left]
+        self.rear_right = Leg(rear_leg_angles, rear_leg_limits, rear_leg_lengths)
+        self.legs = [self.front_left, self.front_right, self.rear_left, self.rear_right]
 
     def _animate(self, positions: list[list[Point]]) -> FuncAnimation:
 
@@ -115,45 +118,131 @@ class Animat:
 
         # TODO: should work with more than one leg (list[list[list[Point]]])
 
+        # TODO: vertical reach should go by length of leg
         horiz_reach = 0.6
         vertical_reach = 0.4
 
-        # Initial position given by current leg position
-        initial_position = self.rear_left.tip_position()
+        # Initial position given by current leg positions
+        positions = [
+            [leg.tip_position()] for leg in self.legs
+        ]  # should be a list[list[pts]]
+
+        # prev version
+        # initial_position = self.rear_left.tip_position()
 
         # Forward motion path
         num_steps = 16
 
-        x, y = initial_position.x, initial_position.y
+        xs = [pos.x for pos in positions]
+        ys = [pos.y for pos in positions]
+        # prev version
+        # x, y = initial_position.x, initial_position.y
         delta_x = horiz_reach / num_steps
         delta_y = 2 * vertical_reach / num_steps
 
-        left_leg_positions = [initial_position]
+        # prev version
+        # left_leg_positions = [initial_position]
+
+        assert len(xs) == len(ys), "Different x and y lengths for legs"
 
         for step in range(num_steps):
-            x += delta_x
-            y += delta_y if step < num_steps // 2 else -delta_y
-            left_leg_positions.append(Point(x=x, y=y))
+
+            for i in range(
+                len(self.legs)
+            ):  # assuming same lengths, we can do it all here
+                xs[i] += delta_x
+                ys[i] += delta_y if step < num_steps // 2 else -delta_y
+                positions[i].append(Point(x=xs[i], y=ys[i]))
+
+            # prev version
+            # x += delta_x
+            # y += delta_y if step < num_steps // 2 else -delta_y
+            # left_leg_positions.append(Point(x=x, y=y))
 
         # Backward motion path
         for _ in range(int(num_steps * 1.5)):
-            x -= delta_x
-            left_leg_positions.append(Point(x=x, y=y))
+            for i in range(len(self.legs)):
+                xs[i] -= delta_x
+                positions[i].append(Point(x=xs[i], y=ys[i]))
+
+            # prev version
+            # x -= delta_x
+            # left_leg_positions.append(Point(x=x, y=y))
 
         # Path back to initial position
         for step in range(num_steps // 2):
-            x += delta_x
-            y += delta_y if step < num_steps // 4 else -delta_y
-            left_leg_positions.append(Point(x=x, y=y))
 
-        positions = [[p.point for p in self.rear_left.global_joint_poses()]]
+            for i in range(len(self.legs)):
+                xs[i] += delta_x
+                ys[i] += delta_y if step < num_steps // 4 else -delta_y
+                positions[i].append(Point(x=xs[i], y=ys[i]))
 
-        # Compute joint angles for each point along th path
-        for goal in left_leg_positions:
-            self.rear_left.move_tip(goal)
-            positions.append([p.point for p in self.rear_left.global_joint_poses()])
+            # prev version
+            # x += delta_x
+            # y += delta_y if step < num_steps // 4 else -delta_y
+            # left_leg_positions.append(Point(x=x, y=y))
 
-        return self._animate(positions) if animate else positions
+        # prev version
+        # positions = [[p.point for p in self.rear_left.global_joint_poses()]]
+
+        initial_pts = self.get_pts_from_gjp()
+
+        # for each leg, we're going to run gjp on it, strip only the points out, and then separate the points into tuples of x,y
+        frames = [
+            [
+                self.split_pts(initial_pts[0]),
+                self.split_pts(initial_pts[1]),
+                self.split_pts(initial_pts[2]),
+                self.split_pts(initial_pts[3]),
+            ]
+        ]  # each frame contains info for one step of all 4 legs
+
+        # Compute joint angles for each point along the path
+        # weird structure bc we want them separated by frames and not by leg
+        for goal_idx in range(len(positions[0])):  # again, assuming all lens same
+
+            for leg_idx, leg in enumerate(self.legs):
+                leg.move_tip(positions[leg_idx][goal_idx])
+
+            all_pts = self.get_pts_from_gjp()
+
+            frames.append(
+                [
+                    self.split_pts(all_pts[0]),
+                    self.split_pts(all_pts[1]),
+                    self.split_pts(all_pts[2]),
+                    self.split_pts(all_pts[3]),
+                ]
+            )
+
+        return self._animate(frames) if animate else frames
+
+        # prev version
+        # for goal in left_leg_positions:
+        #     self.rear_left.move_tip(goal)
+        #     positions.append([p.point for p in self.rear_left.global_joint_poses()])
+        # return self._animate(positions) if animate else positions
+
+    def split_pts(self, pts: List[Point]) -> Tuple[List[float], List[float]]:
+        # helper function, since we can update an actor with all x and y coordinates in this format
+        xs = []
+        ys = []
+        for pt in pts:
+            xs.append(pt.x)
+            ys.append(pt.y)
+
+        return (xs, ys)
+
+    def get_pts_from_gjp(self) -> List[List[Point]]:
+        # runs global_joint_poses on each leg in self.legs and returns a list of points
+        all_pts = []
+
+        for leg in self.legs:
+
+            leg_pts = [pose.point for pose in leg.global_joint_poses()]
+            all_pts.append(leg_pts)
+
+        return all_pts
 
 
 class Leg:
@@ -253,7 +342,7 @@ class Leg:
                 # Compute the new angle and clip within specified limits
                 self.angles[i] = clip(new_angle, lo_limit, hi_limit)
 
-            # Check if close enough to goal
+            # Check if close enough to goa                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    l
             dist = (goal - self.tip_position()).norm
             if abs(dist) < tolerance:
                 break
